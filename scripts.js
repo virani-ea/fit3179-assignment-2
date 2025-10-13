a91beb831632b390d6a007365ae776c05ed7cc5d
virani-ea <161710201+virani-ea@users.noreply.github.com>
2025-10-14 02:14:15 +1100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ function embedVis(elementId, specUrl) {
 
 // Load all visualisations
 embedVis('vis1', 'population_density.vg.json');
-embedVis('vis2', 'vic_lga_seifa.vg.json');
-// embedVis('vis3', 'hospital_state_bar.vg.json');
+embedVis('vis2', 'state_hospital.vg.json');
+embedVis('vis3', 'vic_lga_seifa.vg.json');
 // embedVis('vis4', 'scatter_seifa_density.vg.json');
 // embedVis('vis5', 'admissions_trend.vg.json');
