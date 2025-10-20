c5da3f5cd62d64df1c123794ea6eb4124284a49d
virani-ea <161710201+virani-ea@users.noreply.github.com>
2025-10-20 18:43:36 +1100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -3,7 +3,6 @@ function embedVis(elementId, specUrl) {
   return vegaEmbed(`#${elementId}`, specUrl, { actions: false }).catch(console.error);
 }
 
-let mapView, lollipopView;
 
 // Load all visualisations
 embedVis('vis1', 'population_density.vg.json');
@@ -11,33 +10,10 @@ embedVis('vis2', 'state_hospital.vg.json');
 
 // Embed the SEIFA map (choropleth) and store the view
 embedVis('vis3', 'vic_lga_seifa.vg.json').then(result => {
-  mapView = result.view;
-
-  // After map is embedded, embed the lollipop chart and store the view
-  embedVis('vis4', 'lollipop_ambulance_response.vg.json').then(result => {
-    lollipopView = result.view;
-
-    // Function to sync selection
-    function updateLollipopSelection() {
-    const selectedDeciles = mapView.signal('decile_filter');
-    console.log('Selection:', selectedDeciles);
-
-    // Pull single value as string and use '' as default if none selected
-    const value = (
-      selectedDeciles &&
-      selectedDeciles.Decile &&
-      selectedDeciles.Decile.length > 0
-    ) ? String(selectedDeciles.Decile[0]) : '';
-
-    lollipopView.signal('DecileSel', value).run();
-    }
-
-
-    // Listen for selection changes on the map and update lollipop chart
-    mapView.addSignalListener('decile_filter', updateLollipopSelection);
 
     // Example URL: use the same CSV your Vega chart uses
     const csvUrl = "https://raw.githubusercontent.com/virani-ea/fit3179-assignment-2/refs/heads/main/data/lga_seifa_indexes_vic.csv";
+    console.log("we got here");
 
     // 1. Fetch and parse the CSV
     Papa.parse(csvUrl, {
@@ -73,7 +49,7 @@ embedVis('vis3', 'vic_lga_seifa.vg.json').then(result => {
                 const lga = select.value;
                 const output = document.getElementById('lga-seifa-output');
                 if (lga && lgaScores[lga]) {
-                    output.textContent = `Your LGA SEIFA decile is ${lgaScores[lga].decile} (Score: ${lgaScores[lga].score})`;
+                    output.textContent = `${lga}'s SEIFA decile is ${lgaScores[lga].decile}`;
                 } else {
                     output.textContent = '';
                 }
@@ -82,6 +58,7 @@ embedVis('vis3', 'vic_lga_seifa.vg.json').then(result => {
     });
 
   });
-});
+;
+embedVis('vis4', 'boxplot_response_times.vg.json');
 
 // embedVis('vis5', 'admissions_trend.vg.json');
